756024b06921d344b3f045c82a3a52bcf8ce76a5
Namachivayam2001 <namachivayam10419u25030@gmail.com>
2023-09-07 20:54:32 +0530

    add name atribute in input field

--- back-end/server.js
+++ back-end/server.js
@@ -12,7 +12,7 @@ const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'Newton@2001',
-  database: 'cakeShop',
+  database: 'cakeshop',
   authPlugins: { mysql_clear_password: () => () => Buffer.from('Newton@2001') },
 })
 
@@ -22,8 +22,9 @@ db.connect( err => {
 })
 
 app.post('/submit', (req, res) => {
+  console.log(req.body)
   const {userid, fname, lname, gender, dob, email, phone, password, confirmPassword, terms} = req.body;
-  const insertQuery = 'INSERT INTO registor_form (UserID, First_Name, Last_Name, Gender, DOB, Email, Mobile_Number, Password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+  const insertQuery = `INSERT INTO registor_form (UserID, First_Name, Last_Name, Gender, DOB, Email, Mobile_Number, Password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
   db.query(insertQuery, [userid, fname, lname, gender, dob, email, phone, password], (err, result) => {
       if (err) {
           console.error(err);
